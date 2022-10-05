76f3a1fb10a9791d0c641a34ab081e237d3af901
Evgeniy Loznevoy <evgeniy.loznevoy@yandex.ru>
2022-10-05 14:43:57 +0300

    init

---
+++ src/js/defaultTasks.js
@@ -0,0 +1,7 @@
+export default function defaultTasks() {
+  return {
+    todo: ['Pay rent', 'Buy a new knife'],
+    inProgress: ['Netology homework'],
+    done: ['BHJ Diploma', 'Check email'],
+  };
+}
